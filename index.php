5aaf892146253eb951ec6a622ffadb01f5997817
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-20 17:17:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
                 </a>
                 <ul class="shift-show">
                    <li><a href="../404/index.php"><img src="https://media.discordapp.net/attachments/943856055564251177/944673808651010148/server.png" class="sv"> Server 1</a></li>
-                   <li><a href="../sever2/sever2.php"><img src="https://media.discordapp.net/attachments/943856055564251177/944673808651010148/server.png" class="sv"> Server 2</a></li>
+                   <li><a href="../music/index.php"><img src="https://media.discordapp.net/attachments/943856055564251177/944673808651010148/server.png" class="sv"> Server 2</a></li>
                    <li><a href="#"><img src="https://media.discordapp.net/attachments/943856055564251177/944674321144635442/updatee.png" class="up"> Đang cập nhật</a></li>
                 </ul>
              </li>
