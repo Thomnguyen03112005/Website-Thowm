4b9bbf53476bb8c52b9f2f6d6cfac7293c7d5965
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-20 17:51:32 +0700

    Update index.php

--- music/index.php
+++ music/index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Music Simple</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="./iconn.png" type="image/x-icon">  
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
