847b69610a7b91defac3367c35bf96f839d3ec6f
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-20 17:37:53 +0700

    Add files via upload

--- music/js/music-list.js
+++ music/js/music-list.js
@@ -1,315 +1,318 @@
 let allMusic = [
 
-    {
-        name: "Harley Bird - Home",
-        artist: "Jordan Scho",
-        img: "music-1",
-        src: "music-1"
-    },
-    {
-        name: "Ikson Anywhere – Ikson",
-        artist: "Audio Library",
-        img: "music-2",
-        src: "music-2"
-    },
-    {
-        name: "Beauz & Jvna - Crazy",
-        artist: "Beauz & Jvna",
-        img: "music-3",
-        src: "music-3"
-    },
-    {
-        name: "Hardwind - Want Me",
-        artist: "Mike Archangelo",
-        img: "music-4",
-        src: "music-4"
-    },
-    {
-        name: "Jim - Sun Goes Down",
-        artist: "Jim Yosef x Roy",
-        img: "music-5",
-        src: "music-5"
-    },
-    {
-        name: "Lost Sky - Vision NCS",
-        artist: "NCS Release",
-        img: "music-6",
-        src: "music-6"
-    },
-    {
-      name: "Stay",
-      artist: "The Kid LAROI, Justin Bieber",
-      img: "stay",
-      src: "music-7"
-    },
-    {
-    name: "Có chắc yêu là đây",
+  {
+      name: "Harley Bird - Home",
+      artist: "Jordan Scho",
+      img: "music-1",
+      src: "music-1"
+  },
+  {
+      name: "Ikson Anywhere – Ikson",
+      artist: "Audio Library",
+      img: "music-2",
+      src: "music-2"
+  },
+  {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+  },
+  {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+  },
+  {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+  },
+  {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+  },
+  {
+    name: "Stay",
+    artist: "The Kid LAROI, Justin Bieber",
+    img: "stay",
+    src: "music-7"
+  },
+  {
+  name: "Có chắc yêu là đây",
+  artist: "Sơn Tùng MTP",
+  img: "cochac",
+  src: "music-8"
+},
+{
+    name: "Muộn rồi mà sao còn",
     artist: "Sơn Tùng MTP",
-    img: "cochac",
+    img: "muonroi",
+    src: "muonroi"
+  },
+  {
+    name: "Mind Games",
+    artist: "Sickick",
+    img: "di",
+    src: "abcd"
+  },
+  {
+    name: "Wished 1001",
+    artist: "Kean Nguyen",
+    img: "wishes",
+    src: "abc"
+  },
+
+  {
+    name: "Try Again",
+    artist: "디어 (d.ear), 재현 (Jaehyun)",
+    img: "ab",
+    src: "try"
+  },
+  {
+    name: "Thank u, next",
+    artist: "Ariana Grande",
+    img: "cem",
     src: "music-8"
   },
   {
-      name: "Muộn rồi mà sao còn",
-      artist: "Sơn Tùng MTP",
-      img: "muonroi",
-      src: "muonroi"
-    },
+    name: "Thay đổi",
+    artist: "Lil wuyn",
+    img: "thaydoi",
+    src: "thaydoi"
+  },
+  {
+    name: "Vài câu nói...",
+    artist: "Grey D",
+    img: "vaicaunoi",
+    src: "vaicaunoi"
+  },
+  {
+    name : 'Đường tôi chở em về',
+    artist: "buitruonglinh",
+    img: "duongtoidua",
+    src: "toichoemve"
+  },
+  {
+    name : 'Phải chăng em đã yêu',
+    artist : 'Juky San',
+    img: "phaichang",
+    src: "phaichang"
+  },
+  {
+    name : 'Talking to the moon',
+    artist : 'Bruno Mars',
+    img: "takingmoon",
+    src: "tkinmoon"
+  },
     {
-      name: "Mind Games",
-      artist: "Sickick",
-      img: "di",
-      src: "abcd"
-    },
-    {
-      name: "Wished 1001",
-      artist: "Kean Nguyen",
-      img: "wishes",
-      src: "abc"
-    },
-  
-    {
-      name: "Try Again",
-      artist: "디어 (d.ear), 재현 (Jaehyun)",
-      img: "ab",
-      src: "try"
-    },
-    {
-      name: "Thank u, next",
-      artist: "Ariana Grande",
-      img: "cem",
-      src: "music-8"
-    },
-    {
-      name: "Thay đổi",
-      artist: "Lil wuyn",
-      img: "thaydoi",
-      src: "thaydoi"
-    },
-    {
-      name: "Vài câu nói...",
-      artist: "Grey D",
-      img: "vaicaunoi",
-      src: "vaicaunoi"
-    },
-    {
-      name : 'Đường tôi chở em về',
-      artist: "buitruonglinh",
-      img: "duongtoiduaem",
-      src: "toichoemve"
-    },
-    {
-      name : 'Phải chăng em đã yêu',
-      artist : 'Juky San',
-      img: "phaichang",
-      src: "phaichang"
-    },
-    {
-      name : 'Talking to the moon',
-      artist : 'Bruno Mars',
-      img: "takingmoon",
-      src: "tknimoon"
-    },
-      {
-      name : 'Kiss me more',
-      artist : 'Dojat',
-      img: "kfm",
-      src: "kissfor"
-    },
-    {
-      name : 'You so sexy',
-      artist : 'LikyBo - Kraazy',
-      img : 'youso',
-      src : 'youso'
-    },
-    {
-      name : 'ifly',
-      artist : 'Bazzi',
-      img : 'ccl',
-      src : 'ifly'
-    },
-    {
-      name : 'good 4 u',
-      artist : 'Olivia Rodrigo ',
-      img : 'cv',
-      src : 'good4u'
-    },
-    {
-      name : 'Có hẹn với thanh xuân',
-      artist : 'Monstar',
-      img : 'cohenvoi',
-      src : 'cohen'
-    },
-    {
-      name : 'Mama boy',
-      artist : 'amee',
-      img : 'mama',
-      src : 'mmboy'
-    },
-    {
-      name : 'Play date',
-      artist : 'Melanie Martinez',
-      img : 'play',
-      src : 'playdate'
-    },
-    {
-      name : 'Rót',
-      artist : 'híu',
-      img : 'rot',
-      src : 'rothiu'
-    },
-    {
-      name : 'LoverBoy',
-      artist : 'A-Wall',
-      img : 'b',
-      src : 'loveboy'
-  
-    },
-    {
-      name : 'Bored',
-      artist : 'Billie Eilish',
-      img : 'levi1',
-      src : 'bored'
-  
-    },
-    {
-      name : 'Double take',
-      artist : 'dhruv',
-      img : 'ccc',
-      src : 'double'
-  
-    },
-    {
-      name : "always, i'll care",
-      artist : 'Jeremy Zucker',
-      img : 'alw',
-      src : 'always'
-  
-    },
-    {
-      name : 'infinity',
-      artist : 'James Young',
-      img : 'csl',
-      src : 'Infinity'
-    },
-    {
-      name : 'Dusk Till Dawn',
-      artist : 'ZAYN & Sia',
-      img : 'alc',
-      src : 'aibiet'
-  
-    },
-    {
-      name : 'Thay mọi cô gái yêu anh',
-      artist : 'Amee',
-      img : 'thaymoi',
-      src : 'thayamee'
-  
-    },
-    {
-      name : 'Everything i wanted',
-      artist : 'Billie Eilish',
-      img : 'd',
-      src : 'eiwant',
-  
-    },
-    {
-      name : 'em không hiểu',
-      artist : 'Chang , Minh huy',
-      img : 'emkhonghiu',
-      src : 'emkhonghieu',
-    },
-    {
-      name : 'Để bầu trời nhìn hai ta',
-      artist : 'Hieuthuhai',
-      img : 'debautroi',
-      src : 'bautroihieuthu3',
-    },
-    {
-      name : 'Precious',
-      artist : 'DADUC',
-      img : 'hoa',
-      src : 'precious',
-  
-    },
-    {
-      name : 'Just Friends',
-      artist : 'Audrey Mika',
-      img : 'dd',
-      src : 'JustF',
-    },
-    {
-      name : 'HONESTY',
-      artist : '(Cover BANG YE DAM - 방예담)',
-      img : 'ac',
-      src : 'Hones',
-  
-    },
-    {
-      name : 'Dịu dàng em đến',
-      artist : 'Erik',
-      img : 'diu',
-      src : 'diudangem',
-  
-    },
-    {
-      name : 'necessary girl',
-      artist : 'oceanfromtheblue',
-      img : 'abcc',
-      src : 'oceanfromtheblue',
-  
-    },
-    {
-      name : 'You Right',
-      artist : 'Doja Cat',
-      img : 'a',
-      src : 'youphai',
-    },
-    {
-      name : 'You and I',
-      artist : 'Katy Perry',
-      img : 'you',
-      src : 'banandtoi',
-  
-    },
-    {
-      name : 'Akuma no ko',
-      artist : '「悪魔の子」by Ai Higuchi',
-      img : 'am',
-      src : 'Akuma',
-  
-    },
-    {
-      name : 'Love me like that',
-      artist : 'sam kim',
-      img : 'c',
-      src : 'loveme',
-  
-    },
-    {
-      name : 'Drunk',
-      artist : 'kean',
-      img : 'drunk',
-      src : 'drunk',
-  
-    },
-    {
-      name : 'Đâu đâu cũng là em',
-      artist : 'Yongcaptain',
-      img : 'mavel.png',
-      src : 'daucung',
-  
-    },
-    {
-      name : 'Em có nghe',
-      artist : 'Kha',
-      img : 'emconghe',
-      src : 'emconghe',
-  
-    },
-    {
-      name : 'Rolling',
-      artist : 'Richie D. ICY & AXTROBOY',
-      img : 'rolling',
-      src : 'rolling',
-    },
-  ];
+    name : 'Kiss me more',
+    artist : 'Dojat',
+    img: "kfm",
+    src: "kissfor"
+  },
+  {
+    name : 'You so sexy',
+    artist : 'LikyBo - Kraazy',
+    img : 'youso',
+    src : 'youso'
+  },
+  {
+    name : 'ifly',
+    artist : 'Bazzi',
+    img : 'ccl',
+    src : 'ifly'
+  },
+  {
+    name : 'good 4 u',
+    artist : 'Olivia Rodrigo ',
+    img : 'cv',
+    src : 'good4u'
+  },
+  {
+    name : 'Có hẹn với thanh xuân',
+    artist : 'Monstar',
+    img : 'cohenvoi',
+    src : 'cohen'
+  },
+  {
+    name : 'Mama boy',
+    artist : 'amee',
+    img : 'mama',
+    src : 'mmboy'
+  },
+  {
+    name : 'Play date',
+    artist : 'Melanie Martinez',
+    img : 'play',
+    src : 'playdate'
+  },
+  {
+    name : 'Rót',
+    artist : 'híu',
+    img : 'rot',
+    src : 'rothiu'
+  },
+  {
+    name : 'LoverBoy',
+    artist : 'A-Wall',
+    img : 'b',
+    src : 'loveboy'
+
+  },
+  {
+    name : 'Bored',
+    artist : 'Billie Eilish',
+    img : 'levi1',
+    src : 'bored'
+
+  },
+  {
+    name : 'Double take',
+    artist : 'dhruv',
+    img : 'ccc',
+    src : 'double'
+
+  },
+  {
+    name : "always, i'll care",
+    artist : 'Jeremy Zucker',
+    img : 'alw',
+    src : 'always'
+
+  },
+  {
+    name : 'infinity',
+    artist : 'James Young',
+    img : 'ccl',
+    src : 'Infinity'
+  },
+  {
+    name : 'Dusk Till Dawn',
+    artist : 'ZAYN & Sia',
+    img : 'alc',
+    src : 'aibiet'
+
+  },
+  {
+    name : 'Thay mọi cô gái yêu anh',
+    artist : 'Amee',
+    img : 'thaymoi',
+    src : 'thayamee'
+
+  },
+  {
+    name : 'Everything i wanted',
+    artist : 'Billie Eilish',
+    img : 'd',
+    src : 'eiwant',
+
+  },
+  {
+    name : 'em không hiểu',
+    artist : 'Chang , Minh huy',
+    img : 'emkhonghiu',
+    src : 'emkhonghieu',
+  },
+  {
+    name : 'Để bầu trời nhìn hai ta',
+    artist : 'Hieuthuhai',
+    img : 'debautroi',
+    src : 'bautroihieuthu3',
+  },
+  {
+    name : 'Precious',
+    artist : 'DADUC',
+    img : 'hoa',
+    src : 'precious',
+
+  },
+  {
+    name : 'Just Friends',
+    artist : 'Audrey Mika',
+    img : 'dd',
+    src : 'JustF',
+  },
+  {
+    name : 'HONESTY',
+    artist : '(Cover BANG YE DAM - 방예담)',
+    img : 'ac',
+    src : 'Hones',
+
+  },
+  {
+    name : 'Dịu dàng em đến',
+    artist : 'Erik',
+    img : 'diu',
+    src : 'diudangem',
+
+  },
+  {
+    name : 'necessary girl',
+    artist : 'oceanfromtheblue',
+    img : 'abcc',
+    src : 'oceanfromtheblue',
+
+  },
+  {
+    name : 'You Right',
+    artist : 'Doja Cat',
+    img : 'a',
+    src : 'youphai',
+  },
+  {
+    name : 'You and I',
+    artist : 'Katy Perry',
+    img : 'you',
+    src : 'banandtoi',
+
+  },
+  {
+    name : 'Akuma no ko',
+    artist : '「悪魔の子」by Ai Higuchi',
+    img : 'am',
+    src : 'Akuma',
+
+  },
+  {
+    name : 'Love me like that',
+    artist : 'sam kim',
+    img : 'c',
+    src : 'loveme',
+
+  },
+  {
+    name : 'Drunk',
+    artist : 'kean',
+    img : 'drunk',
+    src : 'drunk',
+
+  },
+  {
+    name : 'Đâu đâu cũng là em',
+    artist : 'Yongcaptain',
+    img : 'mavel.png',
+    src : 'daucung',
+
+  },
+  {
+    name : 'Em có nghe',
+    artist : 'Kha',
+    img : 'emconghe',
+    src : 'emconghe',
+
+  },
+  {
+    name : 'Rolling',
+    artist : 'Richie D. ICY & AXTROBOY',
+    img : 'rolling',
+    src : 'rolling',
+  },
+];
+
+
+
